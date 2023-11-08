1a5401cc35115ba21e5e1ab5b233f719fd1b60c4
Tr4ckTV <theo.a.gregoire@gmail.com>
2023-11-08 15:29:43 +0100

    👀

--- main.js
+++ main.js
@@ -4,21 +4,21 @@ import { data } from './data';
 import { nav } from './nav';
 
 import Fuse from 'fuse.js'
-
+ 
 const fuseExact = new Fuse(data, {
   keys: ['prenom', 'nom'],
   threshold: 0.2,
   minMatchCharLength: 3,
   useExtendedSearch: true,
 });
-
+ 
 const fuseStartsWith = new Fuse(data, {
   keys: ['prenom', 'nom'],
   threshold: 0.1,
   minMatchCharLength: 1,
   useExtendedSearch: true,
 });
-
+ 
 //triage 
 data.sort((a, b) => {
   const prenomA = a.prenom;
@@ -34,10 +34,10 @@ data.sort((a, b) => {
 
 //reste
 
-const listePersonnes = () => {
+const listePersonnes = (Data = data) => {
   let html = '';
-  for (let i = 0; i < data.length; i++) {
-    const personne = data[i];
+  for (let i = 0; i < Data.length; i++) {
+    const personne = Data[i];
     let personneCard = `
       <a class="card col-5 col-md-3" href="/personne/?id=${personne.id}">
         <img src="${personne.avatar}" class="card-img-top" alt="avatar de ${personne.prenom} ${personne.nom}">
@@ -50,7 +50,7 @@ const listePersonnes = () => {
   }
   return html;
 };
-
+ 
 document.querySelector('#app').innerHTML = `
   <main>
     ${nav}
@@ -67,17 +67,17 @@ document.querySelector('#app').innerHTML = `
       </div>
     </div>
   </main>
-
+ 
   <footer>
-    <div>yo</div>
+        <div>yo</div>
   </footer>
 `;
-
+ 
 
 function handleSearch() 
 {
   const searchText = searchInput.value;
-
+ 
   if (searchText === '') 
   {
     document.querySelector('#personList').innerHTML = listePersonnes();
@@ -87,10 +87,10 @@ function handleSearch()
     const resultExact = fuseExact.search(searchText);
     const resultStartsWith = fuseStartsWith.search(searchText);
     const combinedResults = [...resultExact, ...resultStartsWith];
-    const uniqueResults = Array.from(new Set(combinedResults.map((item) => item.item.id)));
-    const filteredData = uniqueResults.map((id) => data.find((item) => item.id === id));
-    document.querySelector('#personList').innerHTML = listePersonnes(filteredData);
+    const uniqueResults = Array.from(new Set(combinedResults.map((item) => item.item.prenom)));
+    const Data = uniqueResults.map((prenom) => data.find((item) => item.prenom === prenom));
+    document.querySelector('#personList').innerHTML = listePersonnes(Data);
   }
 }
-
+ 
 searchInput.addEventListener('input', handleSearch);
